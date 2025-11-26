786cccb658ae4b9281900618a1f4a929d2e9cc0d
Thanveer-Mohammed-Kaif <thanveermohammedkaif1409@gmail.com>
2025-11-26 11:27:01 +0530

    commit

---
+++ Js/slick-min.js
@@ -0,0 +1,177 @@
+$(".responsive").slick({
+  slidesToShow: 3,
+  speed: 2000,
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  nextArrow: false,
+  prevArrow: false,
+  autoplaySpeed: 1000,
+  // slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+        nextArrow: false,
+        prevArrow: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".responsive-2").slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  infinite: true,
+  arrows: false,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".resource-prev").click(function () {
+  $(".responsive-2").slick("slickPrev");
+});
+
+$(".resource-next").click(function () {
+  $(".responsive-2").slick("slickNext");
+});
+
+$(".responsive-3").slick({
+  slidesToShow: 2,
+  speed: 1000,
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  nextArrow: false,
+  prevArrow: false,
+  autoplaySpeed: 1000,
+  // slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        nextArrow: false,
+        prevArrow: false,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".responsive-4").slick({
+  slidesToShow: 3,
+  speed: 1000,
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  nextArrow: false,
+  prevArrow: false,
+  autoplaySpeed: 1000,
+  // slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+        nextArrow: false,
+        prevArrow: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".responsive-5").slick({
+  dots: false,
+  arrows: false, // hide default arrows
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: "linear",
+});
+
+
+$(document).on("click", ".slick-prev-custom", function () {
+  $(".responsive-5").slick("slickPrev");
+});
+
+
+$(document).on("click", ".slick-next-custom", function () {
+  $(".responsive-5").slick("slickNext");
+});
