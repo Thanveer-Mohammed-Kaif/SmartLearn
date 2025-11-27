13e3f7f1ea2f615805bc3bdd94934194569ff9ff
Thanveer-Mohammed-Kaif <thanveermohammedkaif1409@gmail.com>
2025-11-27 10:52:10 +0530

    slick button attached

--- Js/slick-min.js
+++ Js/slick-min.js
@@ -118,44 +118,45 @@ $(".responsive-3").slick({
   ],
 });
 
-$(".responsive-4").slick({
-  slidesToShow: 3,
-  speed: 1000,
-  dots: false,
-  infinite: true,
-  autoplay: true,
-  nextArrow: false,
-  prevArrow: false,
-  autoplaySpeed: 1000,
-  // slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: false,
-        nextArrow: false,
-        prevArrow: false,
+$(document).ready(function () {
+  $(".responsive-4").slick({
+    slidesToShow: 3,
+    speed: 800,
+    dots: false,
+    infinite: true,
+    autoplay: true,          // keep autoplay if you want
+    autoplaySpeed: 1500,
+    arrows: true,            // VERY IMPORTANT
+
+    prevArrow:
+      '<button type="button" class="slick-prev custom-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-next custom-arrow"><i class="fa-solid fa-chevron-right"></i></button>',
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+        },
       },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
       },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
       },
-    },
-  ],
+    ],
+  });
 });
+
 
 $(".responsive-5").slick({
   dots: false,
